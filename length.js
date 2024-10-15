bd629dccf5e556bacee0ac43fb3ed74fe32b6ea0
Jannatul711 <jfmim711@gmail.com>
2024-10-15 12:29:06 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,24 @@
+// const number = [1,2,3,4,5,6,7,8,9];
+// // console.log(number.length); check elements
+// // console.log(number[6]); check index
+// // console.log(number.indexOf(7));
+// number.push(10);
+// console.log(number);
+
+// const team =["Rahim","Karim","Fahim","Mahim"];
+// team.push("Mim");
+// console.log(team);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.unshift("Lemon", "Pineapple");
+// console.log(fruits);add new elements to the beginning
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.pop();
+// console.log(fruits); removes the last element of an array.
+
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruit.shift();
+console.log(fruit);
+// removes the first item of an array.
+
---
+++ main.js
@@ -0,0 +1,9 @@
+
+x = 5;
+y = 6;
+z = x + y;
+console.log(z);
+
+const s = [1,2,3,34,45,56,67,78];
+console.log(s);
+
